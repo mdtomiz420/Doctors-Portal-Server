f6f50cb87e22f066e21ab06942eed05d84c7daa0
mdtomiz420 <tamizrabbi@gmail.com>
2022-05-15 08:32:08 +0600

    May 15, 2022

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion , ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
@@ -19,6 +19,7 @@ async function run() {
     await client.connect()
     console.log('Database Connected')
     const serviceCollection = client.db("doctors_portal").collection("services")
+    const appointmentCollection = client.db("doctors_portal").collection("appointment")
 
     app.get('/services' , async (req , res)=>{
         const query = {};
@@ -26,15 +27,69 @@ async function run() {
         const services = await cursor.toArray();
         res.send(services)
     })
-    
-    // 2118684145 2021 
+
+    app.get('/available' , async (req , res) =>{
+      const date = req.query.date
+      // Get All data From Service collection
+
+      const services = await serviceCollection.find().toArray()
+
+      // Get appointments of That Day
+
+      const query = {date : date}
+
+      const appointments = await appointmentCollection.find(query).toArray()
+
+      // For Each Service
+      services.forEach((service) => {
+        const serviceAppointment = appointments.filter(appoint => appoint.treatment === service.name)
+
+        const appointedSlots = serviceAppointment.map(app => app.slot)
+
+        const available = service.slot.filter(slot1 => !appointedSlots.includes(slot1))
+        console.log(available);
+        service.slot = available
+      });
+
+      res.send(services)
+
+    })
+
+    app.post('/appointment' , async (req, res)=>{
+      const ApData = req.body
+      const query = {date : ApData.date ,slot : ApData.slot , email : ApData.email }
+      const exists = await appointmentCollection.findOne(query)
+      if (exists) {
+        return res.send({ success: false, appointment: exists })
+      }
+      const result = await appointmentCollection.insertOne(ApData)
+
+      res.send({success: true , result})
+    })
+
+    app.get('/appointment' , async (req , res) =>{
+      const email = req.query.email
+      const query = {email : email};
+      const cursor = appointmentCollection.find(query)
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    app.delete('/appointment/:id' , async (req , res) => {
+      const id = req.params.id
+      const result = await appointmentCollection.deleteOne( { "_id" : ObjectId(id) } );
+      console.log(result);
+      res.send({deleted : true})
+    })
+    // 2118684145 2021
 }
 run().catch(console.dir)
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
-  
+
   app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
   })
